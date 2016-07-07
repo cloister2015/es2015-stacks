a57f07f1c2fd078254461ac6ed72df6eec636993
cloister <cloister2015@gmail.com>
2016-07-07 11:01:02 +0900

    set coverge up

--- karma.conf.js
+++ karma.conf.js
@@ -1,24 +1,49 @@
 'use strict';
 
-module.exports = function (config) {
-  config.set({
-    browser: ['Chrome'],
-    reporters: ['mocha'],
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-        loaders: [
-          {
-            test: /(\.js)$/,
-            exclude: /(node_modules)/,
-            loader: ['babel-loader'],
-            query: {
-              presets: ['es2015']
-            }
+module.exports = function(config) {
+  let browsers = ['Chrome'];
+  let reporters = ['mocha'];
+  let preprocessors = ['webpack', 'sourcemap'];
+  let webpack = {
+    devtool : 'inline-source-map',
+    module: {
+      loaders: [
+        {
+          test: /(\.js)$/,
+          exclude: /(node_modules)/,
+          loader: ['babel-loader'],
+          query: {
+            presets: ['es2015']
           }
-        ]
+        }
+      ]
+    }
+  };
+
+  if (config.coverage) {
+    browsers = ['PhantomJS'];
+    reporters = ['mocha', 'coverage'];
+    preprocessors = ['webpack'];
+    webpack.isparta = {
+      embedSource: true,
+      noAutoWrap: true,
+      babel: {
+        presets: ['es2015']
       }
-    },
+    };
+    webpack.module.preLoaders = [
+      {
+        test: /(\.js)$/,
+        exclude: /(__test__|node_modules)/,
+        loader: 'isparta'
+      }
+    ];
+  }
+
+  config.set({
+    browsers: browsers,
+    reporters: reporters,
+    webpack: webpack,
     frameworks: ['mocha'],
     files: [
       './src/**/__tests__/*-test.js'
@@ -29,19 +54,20 @@ module.exports = function (config) {
       'karma-mocha',
       'karma-sourcemap-loader',
       'karma-webpack',
-      'karma-mocha-reporter'
+      'karma-mocha-reporter',
+      'karma-coverage',
+      'karma-phantomjs-launcher'
     ],
     preprocessors: {
-      './src/**/__tests__/*-test.js': ['webpack']
+      './src/**/__tests__/*-test.js': preprocessors
     },
     coverageReporter: {
       type: 'html',
       dir: 'coverage'
     },
     webpackMiddleware: {
-      noInfo: true // please don't spam the console when running in karma.
+      noInfo: true
     },
-    singleRun: false
+    singleRun: config.coverage
   });
 };
-  
